2b998762d9fcde9430bdd485b8c35fd748a26875
Badri Paudel <thecrazymanav@gmail.com>
2024-01-04 17:55:54 +0545

    Code to experiment drools features created.

--- src/main/java/com/app/tracker/utils/PatientUtil.java
+++ src/main/java/com/app/tracker/utils/PatientUtil.java
@@ -16,9 +16,11 @@ public class PatientUtil {
 
     public static List<Patient> getAllPatient() {
         return List.of(
-               new Patient(1, "Shyam", new Address(11, "Swachha Marga", 1100, "Nepal")),
-               new Patient(1, "Ram", new Address(22, "Unnati Marga", 2200, "Nepal")),
-               new Patient(1, "Hathy", new Address(56, "The Hybrid Road", 5600, "The UK"))
+               new Patient(11, "Shyam", new Address(11, "Swachha Marga", 1100, "Nepal")),
+               new Patient(21, "Hari", new Address(11, "KTM Road", 1100, "Nepal")),
+               new Patient(31, "Ram", new Address(22, "Unnati Marga", 2200, "Nepal")),
+               new Patient(41, "Jadeja", new Address(33, "Head Road", 3300, "India")),
+               new Patient(51, "Hathy", new Address(56, "The Hybrid Road", 5600, "India"))
         );
     }
 

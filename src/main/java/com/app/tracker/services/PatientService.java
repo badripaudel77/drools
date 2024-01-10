9d9a4d17656a58cf2f8183342c6fd148bb3f1c21
Badri Paudel <thecrazymanav@gmail.com>
2024-01-10 15:05:49 +0545

    Event Listeners attached to kie session and inspected by writing code in a separate class.  A service created, added as global to the kie session and used to utilize the method written within it.

--- src/main/java/com/app/tracker/services/PatientService.java
+++ src/main/java/com/app/tracker/services/PatientService.java
@@ -1,6 +1,7 @@
 package com.app.tracker.services;
 
 import com.app.tracker.models.Patient;
+import com.app.tracker.utils.EventListenerUtil;
 import com.app.tracker.utils.PatientUtil;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
@@ -38,9 +39,28 @@ public class PatientService {
         List<Patient> patientList = PatientUtil.getAllPatient();
         patientList.forEach( kieSession::insert);
 
+        // Add global so it can be accessed from other places. Here PatientService i.e this is set as a global so that it can be used  to invoke
+        // methods in this service.
+        kieSession.setGlobal("patientService", this);
+
+        // Attach event listeners to kieSession and check the triggered events
+
+        // 1. Attaching DefaultAgendaEventListener
+        EventListenerUtil.attachDefaultAgendaEventListener(kieSession);
+
+        // 2. Attaching RuleRuntimeEventListener
+        EventListenerUtil.attachRuleRunTimeListener(kieSession);
+
+        // 2. Attaching ProcessEventListener
+        EventListenerUtil.attachProcessEventListener(kieSession);
+
         int firedCount = kieSession.fireAllRules();
         logger.info(firedCount + " {} objects fired.");
 
         kieSession.dispose();
+    }
+
+    public void logTheMessage() {
+        this.logger.info("Logging from the service to inspect the code.");
     }
 }
